76757b3c0569daf53fbf1d78ff98d3d292583a7e
Kyle Raney <raney24fcb@gmail.com>
2019-11-26 02:01:11 -0500

    pub changes

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/willshapiro/Documents/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/willshapiro/Documents/development/five_across"
-export "FLUTTER_TARGET=/Users/willshapiro/Documents/development/five_across/lib/main.dart"
+export "FLUTTER_ROOT=/Users/kyle/Programming/lib/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/kyle/Programming/Miscellaneous/accountability"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/willshapiro/Documents/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/kyle/Programming/lib/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
